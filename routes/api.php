eb191ba7cee347008b7df71e39584c95f8cdc064
doraeem <mehzebin.meem@gmail.com>
2024-11-29 15:11:42 +0600

    update files
    update .env , added blog, image upload

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,14 @@
 <?php
 
 use App\Http\Controllers\BlogController;
-// use App\Http\Controllers\TempImageController;
+use App\Http\Controllers\TempImageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+
 Route::post('blogs', [BlogController::class, 'store']);
+Route::post('save-temp-image',[TempImageController::class,'store']);
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
